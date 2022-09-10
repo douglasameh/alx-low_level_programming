c23871ec71b89c168d8139fc224665494cbf6b18
Douglas <douglasekwule@gmail.com>
2022-09-09 19:42:31 -0700

    fixed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,23 +7,26 @@
  * 
  * Return: returns zero at the end
  */
-int main()
+
+int main(void)
+
 {
 	int n1 = 0, n2;
-	while(n1 <= 99)
+
+	while (n1 <= 99)
 	{
 		n2 = n1;
-		while(n2 <= 99)
+		while (n2 <= 99)
 		{
-			if(n2 != n1)
+			if (n2 != n1)
 			{
-				putchar((n1 / 10) + 48);
-				putchar((n1 % 10) + 48);
-				putchar(' ');
-				putchar((n2 / 10) + 48);
-				putchar((n2 % 10) + 48);
+				putchar ((n1 / 10) + 48);
+				putchar ((n1 % 10) + 48);
+				putchar (' ');
+				putchar ((n2 / 10) + 48);
+				putchar ((n2 % 10) + 48);
 
-				if(n1 != 98 || n2 != 99)
+				if (n1 != 98 || n2 != 99)
 				{
 					putchar(',');
 					putchar(' ');
@@ -34,5 +37,5 @@ int main()
 		++n1;
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
